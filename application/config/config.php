f4de7919148d0e58365a0fa62931cf5a4aa3dded
ffadilaputra <i.fadilaputra@gmail.com>
2018-10-09 15:23:45 +0700

    Manuk :monkey:

--- application/config/config.php
+++ application/config/config.php
@@ -26,8 +26,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $config['base_url'] = 'http://localhost/webdevelopment/works/spk_sekolah/';
 $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
-$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-
+if (!isset($_SERVER['ORIG_SCRIPT_NAME'])){
+  $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+}else{
+  $config['base_url'] .= str_replace(basename($_SERVER['ORIG_SCRIPT_NAME']),"",$_SERVER['ORIG_SCRIPT_NAME']);
+}
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -373,7 +376,7 @@ $config['encryption_key'] = '13rabka04';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
